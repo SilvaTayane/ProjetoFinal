de3b21a99f4d61c0df205738179b3cf0ed410112
Tayane Emanuely da Silva <146960458+SilvaTayane@users.noreply.github.com>
2025-05-19 18:43:18 -0400

    fly_bad

--- haven/settings.py
+++ haven/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-lb%i37s-a3+ux01iw!o-=h09lgz#ei#o=ed23s#8akyxlpcph7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
